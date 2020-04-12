4f82889049ed2e3456569efa15101de93415291d
Paweł Karpiński <pawelka@gmail.com>
2020-04-12 08:08:12 +0200

    Update run.sh

--- supla-mqtt-integration/run.sh
+++ supla-mqtt-integration/run.sh
@@ -6,20 +6,18 @@ cd /supla-mqtt-client
 
 cat <<EOF > generated-config.yaml
 mqtt:
-  host: '$(jq --raw-output ".mqtt.host" $CONFIG_FILE)'
-  port: $(jq --raw-output ".mqtt.port" $CONFIG_FILE || echo "1883")
-  commands_file_path: '$(jq --raw-output ".mqtt.commands_file_path" $CONFIG_FILE || echo "config-supla/command.yaml")'
-  states_file_path: '$(jq --raw-output ".mqtt.states_file_path" $CONFIG_FILE || echo "config-supla/state.yaml")'
-  client_name: '$(jq --raw-output ".mqtt.client_name" $CONFIG_FILE || echo "supla_mqtt_client")'
-  protocol_version: $(jq --raw-output ".mqtt.protocol_version" $CONFIG_FILE || echo "3")
-  publish_supla_events: $(jq --raw-output ".mqtt.publish_supla_events" $CONFIG_FILE || echo "true")
-  username: $(jq --raw-output ".mqtt.username" $CONFIG_FILE || echo "")
-  password: $(jq --raw-output ".mqtt.password" $CONFIG_FILE || echo "")
+  host: '$(jq --raw-output '.mqtt.host' $CONFIG_FILE)'
+  port: $(jq --raw-output '.mqtt.port // "1883"' $CONFIG_FILE)
+  commands_file_path: '$(jq --raw-output '.mqtt.commands_file_path // "config-supla/command.yaml"' $CONFIG_FILE)'
+  states_file_path: '$(jq --raw-output '.mqtt.states_file_path // "config-supla/state.yaml"' $CONFIG_FILE)'
+  client_name: '$(jq --raw-output '.mqtt.client_name // "supla_mqtt_client"' $CONFIG_FILE)'
+  protocol_version: $(jq --raw-output '.mqtt.protocol_version // "3"' $CONFIG_FILE)
+  publish_supla_events: $(jq --raw-output '.mqtt.publish_supla_events // "true"' $CONFIG_FILE)
+  username: $(jq --raw-output '.mqtt.username // ""' $CONFIG_FILE)
+  password: $(jq --raw-output '.mqtt.password // ""' $CONFIG_FILE)
 supla:
-  port: $(jq --raw-output ".supla.port" $CONFIG_FILE || echo "2016")
-  host: '$(jq --raw-output ".supla.host" $CONFIG_FILE || echo "192.168.1.3")'
-  protocol_version: $(jq --raw-output ".supla.protocol_version" $CONFIG_FILE || echo "10")
-  email: '$(jq --raw-output ".supla.email" $CONFIG_FILE || echo "yourmail@here.com")'
+  port: $(jq --raw-output '.supla.port // "2016"' $CONFIG_FILE)
+  host: '$(jq --raw-output '.supla.host' $CONFIG_FILE)'
+  protocol_version: $(jq --raw-output '.supla.protocol_version // "10"' $CONFIG_FILE)
+  email: '$(jq --raw-output '.supla.email' $CONFIG_FILE)'
 EOF
-
-./supla-mqtt-client -config generated-config.yaml
